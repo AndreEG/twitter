96d6a4667e907d471b5db03988f9f2e21f97d27b
AndreEG <andrea_206_93@hotmail.com>
2017-11-20 08:02:55 -0500

    añade eventos de focus y click

--- js/app.js
+++ js/app.js
@@ -1,24 +1,40 @@
+var textArea = document.getElementById('section-tweet');
+var countArea = document.getElementById('p-count');
+var icons = document.getElementById('icons');
+var btn = document.getElementById('btn-twittear');
+var ul = document.getElementById('tweet-here').firstElementChild;
+
 window.addEventListener('load', function() {
-  var textArea = document.getElementById('section-tweet');
-  var icons = document.getElementById('icons');
-  var btn = document.getElementsByClassName('submit-none')[0];
-  textArea.addEventListener('focus', function() {
-    textArea.classList.add('section-tweet-focus');
-    textArea.classList.remove('section-tweet');
-    icons.classList.add('icons-focus');
-    icons.classList.remove('icons');
-    btn.classList.add('submit');
-    btn.classList.remove('submit-none');
+  textArea.addEventListener('focus', fc);
+  // textArea.addEventListener('blur', blr); falta validar que haga blur cuando el usuario no haya igresado texto
+  btn.addEventListener('click', function() {
+    if (textArea.value) {
+      var li = document.createElement('li');
+      li.textContent = textArea.value;
+      ul.appendChild(li);
+      textArea.value = '';
+    };
   });
-  textArea.addEventListener('blur', function() {
-    textArea.classList.remove('section-tweet-focus');
-    textArea.classList.add('section-tweet');
-    icons.classList.remove('icons-focus');
-    icons.classList.add('icons');
-    btn.classList.remove('submit');
-    btn.classList.add('submit-none');
-  });
-  btn.addEventListener('click',function(e) {
-    
+  window.addEventListener('keyup', function() {
+    var count = textArea.value.length;
+    countArea.textContent = 140 - count;
   });
 });
+
+var fc = function() {
+  textArea.classList.add('section-tweet-focus');
+  textArea.classList.remove('section-tweet');
+  icons.classList.add('icons-focus');
+  icons.classList.remove('display-none');
+  countArea.classList.add('area-count');
+  countArea.classList.remove('display-none');
+};
+var blr = function() {
+  textArea.classList.remove('section-tweet-focus');
+  textArea.classList.add('section-tweet');
+  icons.classList.remove('icons-focus');
+  icons.classList.add('display-none');
+  countArea.classList.remove('area-count');
+  countArea.classList.add('display-none');
+};
+
