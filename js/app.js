a908e3dcf4a212d8c1c1bd709ee92c8057fa9e5c
AndreEG <andrea_206_93@hotmail.com>
2017-11-21 17:44:48 -0500

    recuperando js

--- js/app.js
+++ js/app.js
@@ -8,25 +8,46 @@ window.addEventListener('load', function() {
   textArea.addEventListener('focus', fc);
   // textArea.addEventListener('blur', blr); falta validar que haga blur cuando el usuario no haya igresado texto
   btn.addEventListener('click', function() {
-    if (textArea.value) {
-      var li = document.createElement('li');
-      li.textContent = textArea.value;
-      ul.appendChild(li);
-      textArea.value = '';
-    };
+    event.preventDefault();
+    var li = document.createElement('li');
+    var hours = moment().format('LT');
+    var fechaCompleta = document.createTextNode(hours);
+    var espacio = document.createTextNode(' ');
+    
+    li.textContent = textArea.value;
+    ul.insertBefore(li, ul.firstElementChild);
+    li.appendChild(espacio);
+    li.appendChild(fechaCompleta);
+
+    textArea.focus();
+    textArea.value = '';
+    tweetBtn.disabled = true;
+    countArea.textContent = 140;
   });
-  window.addEventListener('keyup', function() {
-    var count = textArea.value.length;
-    countArea.textContent = 140 - count;
-    if (countArea.textContent <= 10) {
-      countArea.classList.add('count-red');
-      countArea.classList.remove('count-orange');
-    } else if (countArea.textContent <= 20) {
-      countArea.classList.add('count-orange');      
+  textArea.addEventListener('keydown', function() {
+    if (event.target.value.trim().length) {
+      var total = 140 - event.target.value.trim().length;
+      countArea.textContent = 140 - event.target.value.trim().length;
+      btn.disabled = false;
+      if (event.target.value.trim().length > 140) {
+        btn.disabled = true;
+      }
+      countArea.classList.toggle('count-orange', 10 <= total && total < 20);
+      countArea.classList.toggle('count-red', 10 > total);
     } else {
-      countArea.classList.remove('count-red');
-      countArea.classList.remove('count-orange');
+      btn.disabled = true;
+      countArea.textContent = 140;
     };
+    /* var arrText = event.target.value.split('');// convierte el valor de texto e un array
+    var acum = 0;// es un acumulador de enters por decirlo
+    for (var i = 0; i < arrText.length; i++) { // recorre el array creado
+      if (text[i] === '\n') { // busca un enter
+        acum++; // cuando lo encuentra acumula
+      }
+      if (acum) {
+        event.target.rows = acum + 2; // si el acumulador es mas de 0 el tamaño agranda
+      }
+    }; */
   });
 });
 
@@ -46,4 +67,3 @@ var blr = function() {
   countArea.classList.remove('area-count');
   countArea.classList.add('display-none');
 };
-
